be55ac9639135cb43915ee83c3ece26532b2fac9
Paul Smith <mail@paulsmith.site>
2022-06-09 14:36:26 +0100

    lint fix

--- application/blueprints/base/views.py
+++ application/blueprints/base/views.py
@@ -6,4 +6,9 @@ base = Blueprint("base", __name__)
 @base.route("/")
 @base.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("homepage.html")
+
+
+@base.route("/organisation")
+def org_summary():
+    return render_template("organisation-summary.html")
